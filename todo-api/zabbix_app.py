2007c03a7558f23b90a313fa731dc48ab126d2fd
lizhao2 <lizhao2@chanjet.com>
2018-01-14 20:55:13 +0800

    scrapy 编写初步循环爬 伯乐在线的网页内容

---
+++ todo-api/zabbix_app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/12 下午4:59
+# @Author  : lee
+# @File    : zabbix_app.py
+# @Version : 1.0
+# 说明: code后有'#'是测试时加的或者需要修改的code
+# 用法：
+
